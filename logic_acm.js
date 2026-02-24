6570a2490bc7efbf8dbd422aad4c87257747aa14
Sign_Store_ERP <evan@signstoremacon.com>
2026-02-24 10:48:25 -0500

    Update logic_acm.js
    update object.keys

--- logic_acm.js
+++ logic_acm.js
@@ -15,9 +15,10 @@ function calculateACM(inputs, data) {
     let bestFitArea = Infinity;
     let bestP1 = null, bestP10 = null, bestLabel = "";
 
+// Bounding Box Search
     Object.keys(data).forEach(key => {
         if (key.startsWith(`RET_ACM${thickStr}_`) && key.endsWith(`_${sideStr}_1`)) {
-            const dimStr = key.split('_')[2]; // Changed from [1] to [2]
+            const dimStr = key.split('_')[2]; 
             const stdShort = parseInt(dimStr.substring(0, 2), 10);
             const stdLong = parseInt(dimStr.substring(2), 10);
             const stdArea = stdShort * stdLong;
@@ -25,7 +26,7 @@ function calculateACM(inputs, data) {
             if (reqShort <= stdShort && reqLong <= stdLong && stdArea < bestFitArea) {
                 bestFitArea = stdArea;
                 bestP1 = parseFloat(data[key]);
-                bestP10 = parseFloat(data[key.replace('_1', '_10')]);
+                bestP10 = parseFloat(data[key.replace(/_1$/, '_10')]) || bestP1;
                 bestLabel = `${stdShort}x${stdLong}`;
             }
         }
